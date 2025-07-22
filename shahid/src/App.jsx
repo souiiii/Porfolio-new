d307ead8280da81589ad45d93b5f55adc309d19b
souiiii <b423037@iiit-bh.ac.in>
2025-07-23 01:08:11 +0530

    added my resume

--- shahid/src/App.jsx
+++ shahid/src/App.jsx
@@ -14,37 +14,66 @@ export default function App() {
               <p className="job">Web Designer and Developer</p>
             </div>
           </div>
-          <a
-            href="mailto:b423037@iiit-bh.ac.in?subject=Inquiry%20from%20Portfolio&body=Hi%20Shahid,%20I%20saw%20your%20portfolio..."
-            target="_blank"
-            className="inquiry"
-          >
-            <span>Inquiry</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="19"
-              height="19"
-              viewBox="0 0 19 19"
-              fill="none"
-            >
-              <g clip-path="url(#clip0_28_113)">
-                <path
-                  d="M13.6682 5.4886C13.6682 5.01215 13.2813 4.62528 12.8049 4.62528L6.70642 4.62528C6.22997 4.62528 5.8431 5.01215 5.8431 5.4886C5.8431 5.96504 6.22997 6.35191 6.70642 6.35191L10.7257 6.35001L4.87878 12.1969C4.54146 12.5342 4.54146 13.0793 4.87878 13.4166C5.2161 13.7539 5.76115 13.7539 6.09848 13.4166L11.9435 7.57161L11.9454 11.5871C11.9454 12.0635 12.3323 12.4504 12.8087 12.4504C13.2852 12.4504 13.672 12.0635 13.672 11.5871L13.672 5.4886H13.6682Z"
-                  fill="#838383"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_28_113">
-                  <rect
-                    width="12.0744"
-                    height="13.7993"
-                    fill="white"
-                    transform="translate(0 8.53784) rotate(-45)"
+          <div className="twoDiv">
+            <a href="/resume-shahid.pdf" target="_blank" className="inquiry">
+              <span>Resume</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="19"
+                height="19"
+                viewBox="0 0 19 19"
+                fill="none"
+              >
+                <g clip-path="url(#clip0_28_113)">
+                  <path
+                    d="M13.6682 5.4886C13.6682 5.01215 13.2813 4.62528 12.8049 4.62528L6.70642 4.62528C6.22997 4.62528 5.8431 5.01215 5.8431 5.4886C5.8431 5.96504 6.22997 6.35191 6.70642 6.35191L10.7257 6.35001L4.87878 12.1969C4.54146 12.5342 4.54146 13.0793 4.87878 13.4166C5.2161 13.7539 5.76115 13.7539 6.09848 13.4166L11.9435 7.57161L11.9454 11.5871C11.9454 12.0635 12.3323 12.4504 12.8087 12.4504C13.2852 12.4504 13.672 12.0635 13.672 11.5871L13.672 5.4886H13.6682Z"
+                    fill="#838383"
                   />
-                </clipPath>
-              </defs>
-            </svg>
-          </a>
+                </g>
+                <defs>
+                  <clipPath id="clip0_28_113">
+                    <rect
+                      width="12.0744"
+                      height="13.7993"
+                      fill="white"
+                      transform="translate(0 8.53784) rotate(-45)"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+            </a>
+            <a
+              href="mailto:b423037@iiit-bh.ac.in?subject=Inquiry%20from%20Portfolio&body=Hi%20Shahid,%20I%20saw%20your%20portfolio..."
+              target="_blank"
+              className="inquiry inq"
+            >
+              <span>Inquiry</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="19"
+                height="19"
+                viewBox="0 0 19 19"
+                fill="none"
+              >
+                <g clip-path="url(#clip0_28_113)">
+                  <path
+                    d="M13.6682 5.4886C13.6682 5.01215 13.2813 4.62528 12.8049 4.62528L6.70642 4.62528C6.22997 4.62528 5.8431 5.01215 5.8431 5.4886C5.8431 5.96504 6.22997 6.35191 6.70642 6.35191L10.7257 6.35001L4.87878 12.1969C4.54146 12.5342 4.54146 13.0793 4.87878 13.4166C5.2161 13.7539 5.76115 13.7539 6.09848 13.4166L11.9435 7.57161L11.9454 11.5871C11.9454 12.0635 12.3323 12.4504 12.8087 12.4504C13.2852 12.4504 13.672 12.0635 13.672 11.5871L13.672 5.4886H13.6682Z"
+                    fill="#838383"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_28_113">
+                    <rect
+                      width="12.0744"
+                      height="13.7993"
+                      fill="white"
+                      transform="translate(0 8.53784) rotate(-45)"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+            </a>
+          </div>
         </div>
         <div className="content">
           <div className="firstbox">
